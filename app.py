12aa264011fca685cac87aa009d8fd7f5d031dcd
Mafaz Ahmed <150810540+mafazgithub@users.noreply.github.com>
2023-12-05 10:44:24 +0400

    app.py

--- app.py
+++ app.py
@@ -1,88 +1,51 @@
 from flask import Flask, request, send_file
 from flask_restful import Resource, Api
-from flask_uploads import UploadSet, configure_uploads, IMAGES, DOCUMENTS
-from flask_restful_swagger_3 import Api as SwaggerApi, Resource as SwaggerResource
+from werkzeug.utils import secure_filename
+import os
 
 app = Flask(__name__)
 api = Api(app)
-swagger = SwaggerApi(app)
 
-# Configure file uploads
-files = UploadSet("files", IMAGES + DOCUMENTS)
-app.config["UPLOADED_FILES_DEST"] = "uploads"
-configure_uploads(app, files)
+# Set the upload folder
+UPLOAD_FOLDER = "uploads"
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
 
-class FileResource(SwaggerResource):
-    @swagger.doc({
-        "tags": ["File Management"],
-        "description": "Download a file",
-        "parameters": [
-            {"name": "filename", "in": "path", "required": True, "description": "Name of the file"},
-        ],
-        "responses": {"200": {"description": "File downloaded successfully"}},
-    })
+class FileResource(Resource):
     def get(self, filename):
         try:
-            return send_file(f"uploads/{filename}")
+            return send_file(os.path.join(app.config["UPLOAD_FOLDER"], filename))
         except Exception as e:
             return {"error": str(e)}, 404
 
-    @swagger.doc({
-        "tags": ["File Management"],
-        "description": "Delete a file",
-        "parameters": [
-            {"name": "filename", "in": "path", "required": True, "description": "Name of the file"},
-        ],
-        "responses": {"200": {"description": "File deleted successfully"}},
-    })
     def delete(self, filename):
         try:
             # Add logic for deleting the file from storage
+            os.remove(os.path.join(app.config["UPLOAD_FOLDER"], filename))
             return {"message": f"{filename} deleted successfully"}
         except Exception as e:
             return {"error": str(e)}, 500
 
 
-class FileListResource(SwaggerResource):
-    @swagger.doc({
-        "tags": ["File Management"],
-        "description": "List all available files",
-        "responses": {"200": {"description": "List of available files"}},
-    })
+class FileListResource(Resource):
     def get(self):
         # Add logic for listing available files
         files_list = []  # Replace with actual list of files
         return {"files": files_list}
 
-    @swagger.doc({
-        "tags": ["File Management"],
-        "description": "Upload a file",
-        "parameters": [
-            {"name": "file", "in": "formData", "required": True, "type": "file", "description": "File to upload"},
-        ],
-        "responses": {"200": {"description": "File uploaded successfully"}},
-    })
     def post(self):
         try:
             file = request.files["file"]
             if file:
-                filename = files.save(file)
+                filename = secure_filename(file.filename)
+                file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
                 # Add logic for storing file details in the database
                 return {"message": f"{filename} uploaded successfully"}
         except Exception as e:
             return {"error": str(e)}, 500
 
 
-class FileShareResource(SwaggerResource):
-    @swagger.doc({
-        "tags": ["File Management"],
-        "description": "Share a file",
-        "parameters": [
-            {"name": "filename", "in": "path", "required": True, "description": "Name of the file"},
-        ],
-        "responses": {"200": {"description": "File shared successfully"}},
-    })
+class FileShareResource(Resource):
     def post(self, filename):
         # Add logic for sharing the file
         # This might involve generating a unique link or managing access permissions
